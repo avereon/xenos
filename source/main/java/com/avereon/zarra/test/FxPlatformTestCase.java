88158cbf9eb8f75cc94364bae845a9097f77c4b2
Mark Soderquist <mark@soderquist.net>
2020-08-09 11:56:42 -0600

    Move FxPlatformTestCase to zarra project

---
+++ source/main/java/com/avereon/zarra/test/FxPlatformTestCase.java
@@ -0,0 +1,13 @@
+package com.avereon.zarra.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public abstract class FxPlatformTestCase extends ApplicationTest {
+
+	@BeforeEach
+	public void setup() throws Exception {
+		//JavaFxStarter.startAndWait( 5000 );
+	}
+
+}
--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -1,5 +1,8 @@
 module com.avereon.zarra {
 
+	requires transitive org.junit.jupiter.api;
+	requires transitive org.testfx;
+	requires transitive org.testfx.junit5;
 	requires javafx.graphics;
 	requires org.hamcrest;
 
