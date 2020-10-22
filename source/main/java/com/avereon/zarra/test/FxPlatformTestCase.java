6be39a2a66b044531ddbce1ec5ba0b6662b03be7
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-10-22 14:04:14 -0600

    Fix method access

--- source/main/java/com/avereon/zarra/test/FxPlatformTestCase.java
+++ source/main/java/com/avereon/zarra/test/FxPlatformTestCase.java
@@ -6,7 +6,7 @@ import org.testfx.framework.junit5.ApplicationTest;
 public abstract class FxPlatformTestCase extends ApplicationTest {
 
 	@BeforeEach
-	public void setup() throws Exception {
+	protected void setup() throws Exception {
 		//JavaFxStarter.startAndWait( 5000 );
 	}
 
