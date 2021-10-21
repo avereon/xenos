d56b15e33ff9cf1357a6ad48b294550ab2e14bac
msoderquist <msoderquist@mastercontrol.com>
2021-10-21 09:36:48 -0600

    Update version numbers and dependency strategy

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.avereon.zerra {
 
 	requires transitive org.junit.jupiter.api;
-	requires transitive org.testfx;
-	requires transitive org.testfx.junit5;
+	requires org.testfx;
+	requires org.testfx.junit5;
 	requires javafx.graphics;
 	requires org.hamcrest;
 
