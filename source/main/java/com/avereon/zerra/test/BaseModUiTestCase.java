ae9da6f5b85a4ff1ddf6f11ce2e08d4ea982027d
msoderquist <msoderquist@mastercontrol.com>
2021-11-23 15:15:58 -0700

    Move xenon unit tests back out from under the 'test' package

---
+++ source/main/java/com/avereon/zerra/test/BaseModUiTestCase.java
@@ -0,0 +1,25 @@
+package com.avereon.zerra.test;
+
+import com.avereon.product.ProductCard;
+import com.avereon.xenon.Mod;
+import lombok.CustomLog;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@CustomLog
+public abstract class BaseModUiTestCase extends BaseXenonUiTestCase {
+
+	private Mod mod;
+
+	protected void initMod( ProductCard card ) {
+		this.mod = getProgram().getProductManager().getMod( card.getProductKey() );
+
+		getProgram().getProductManager().setModEnabled( card, true );
+		assertThat( getProgram().getProductManager().isEnabled( card ) ).withFailMessage( "Module not ready for testing: " + mod ).isTrue();
+	}
+
+	protected Mod getMod() {
+		return mod;
+	}
+
+}
--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -1,10 +1,18 @@
 module com.avereon.zerra {
 
-	requires transitive org.junit.jupiter.api;
+	// Compile-time only
+	requires static lombok;
+
+	// Both compile-time and run-time
+	requires com.avereon.xenon;
+	requires org.assertj.core;
+	requires org.junit.jupiter.api;
 	requires org.testfx;
 	requires org.testfx.junit5;
+
+	requires java.logging;
 	requires javafx.graphics;
 
-	//exports com.avereon.zerra.test;
+	exports com.avereon.zerra.test;
 
 }
