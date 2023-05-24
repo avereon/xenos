7e6b9f4c7a73299518363ef679a924c8963f0f6e
msoderquist <msoderquist@mastercontrol.com>
2023-05-24 15:33:05 -0600

    Cleanup testing strategy

--- source/main/java/com/avereon/zerra/BaseFxPlatformTestCase.java
+++ source/main/java/com/avereon/zerra/BaseFxPlatformTestCase.java
@@ -12,9 +12,7 @@ public abstract class BaseFxPlatformTestCase extends BaseForAllTests {
 		// NOTE Do not create an application for tests that only need the FX platform
 
 		// Start the FX platform
-		System.out.println( "Before Fx.startup()" );
 		Fx.startup();
-		System.out.println( "After Fx.startup()" );
 	}
 
 }
