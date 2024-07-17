189f2d21f0b9ae23f27ab981f108f2c575ea1411
msoderquist <msoderquist@mastercontrol.com>
2024-07-17 15:52:15 -0600

    Test class cleanup

--- source/main/java/com/avereon/zerra/BaseModTestCase.java
+++ source/main/java/com/avereon/zerra/BaseModTestCase.java
@@ -3,7 +3,7 @@ package com.avereon.zerra;
 import com.avereon.xenon.Module;
 import org.junit.jupiter.api.BeforeEach;
 
-public class BaseModTestCase<T extends Module> extends BaseXenonTestCase {
+public class BaseModTestCase<T extends Module> extends BasePartXenonTestCase {
 
 	private final Class<T> type;
 
@@ -16,6 +16,9 @@ public class BaseModTestCase<T extends Module> extends BaseXenonTestCase {
 	@BeforeEach
 	protected void setup() throws Exception{
 		super.setup();
+
+		// FIXME This works when its the full application, not for part
+		// But we want it to work for part application tests
 		//this.module = CommonModTestStuff.initMod( getProgram(), ProductCard.card( type ) );
 	}
 
--- source/main/java/com/avereon/zerra/CommonModTestStuff.java
+++ source/main/java/com/avereon/zerra/CommonModTestStuff.java
@@ -10,6 +10,7 @@ public interface CommonModTestStuff {
 
 	static Module initMod( Xenon program, ProductCard card ) {
 		Module module = program.getProductManager().getMod( card.getProductKey() );
+		assertThat( module ).withFailMessage( "Module not found: " + card ).isNotNull();
 
 		program.getProductManager().setModEnabled( card, true );
 		assertThat( program.getProductManager().isEnabled( card ) ).withFailMessage( "Module not ready for testing: " + module ).isTrue();
