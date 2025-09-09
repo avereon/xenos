57cfdda61b05af8ff957584ed8a306e4cfbd681e
Mark Soderquist <mark@soderquist.net>
2025-09-08 19:00:41 -0600

    Cleanup

--- source/main/java/com/avereon/xenos/BaseToolUIT.java
+++ source/main/java/com/avereon/xenos/BaseToolUIT.java
@@ -1,23 +1,3 @@
 package com.avereon.xenos;
 
-import com.avereon.xenon.workpane.Tool;
-import com.avereon.xenon.workpane.Workpane;
-
-import java.util.Collection;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-public abstract class BaseToolUIT extends BaseXenonUIT {
-
-	protected void assertToolCount( Workpane pane, int count ) {
-		Collection<Tool> tools = pane.getTools();
-
-		try {
-			assertThat( tools.size() ).isEqualTo( count );
-		} catch( AssertionError error ) {
-			tools.forEach( t -> System.out.println( "Tool: " + t ) );
-			throw error;
-		}
-	}
-
-}
+public abstract class BaseToolUIT extends BaseXenonUIT {}
